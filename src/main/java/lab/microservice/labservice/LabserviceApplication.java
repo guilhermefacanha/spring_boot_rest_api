8a809ac6dd3b6d97b37977c5e69c023deed72136
guilhermefacanha <guilhermefacanha@gmail.com>
2018-08-16 22:43:18 -0700

    Initial commit skeleton

---
+++ src/main/java/lab/microservice/labservice/LabserviceApplication.java
@@ -0,0 +1,12 @@
+package lab.microservice.labservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/lab/microservice/labservice/LabserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package lab.microservice.labservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes=LabserviceApplication.class)
+public class LabserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
